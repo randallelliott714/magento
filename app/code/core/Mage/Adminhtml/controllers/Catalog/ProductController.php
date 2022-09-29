randallelliott714/magento

commit a9a0fc84fef44d80578295c766e839901b3c67c2

PHPStan: fixes for Mage_Catalog_Model_Product_Type_Abstract (#2605)

app/code/core/Mage/Adminhtml/controllers/Catalog/ProductController.php
@@ -98,11 +98,14 @@ class Mage_Adminhtml_Catalog_ProductController extends Mage_Adminhtml_Controller
         }
 
         $attributes = $this->getRequest()->getParam('attributes');
-        if ($attributes && $product->isConfigurable() &&
-            (!$productId || !$product->getTypeInstance()->getUsedProductAttributeIds())) {
-            $product->getTypeInstance()->setUsedProductAttributeIds(
-                explode(",", base64_decode(urldecode($attributes)))
-            );
+        if ($attributes && $product->isConfigurable()) {
+            /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+            $productType = $product->getTypeInstance();
+            if (!$productId || !$productType->getUsedProductAttributeIds()) {
+                $productType->setUsedProductAttributeIds(
+                    explode(",", base64_decode(urldecode($attributes)))
+                );
+            }
         }
 
         // Required attributes of simple product for configurable creation
@@ -207,10 +210,12 @@ class Mage_Adminhtml_Catalog_ProductController extends Mage_Adminhtml_Controller
             $this->loadLayout('popup');
         } else {
             $_additionalLayoutPart = '';
-            if ($product->getTypeId() == Mage_Catalog_Model_Product_Type::TYPE_CONFIGURABLE
-                && !($product->getTypeInstance()->getUsedProductAttributeIds()))
-            {
-                $_additionalLayoutPart = '_new';
+            if ($product->getTypeId() === Mage_Catalog_Model_Product_Type::TYPE_CONFIGURABLE) {
+                /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+                $productType = $product->getTypeInstance();
+                if (!$productType->getUsedProductAttributeIds()) {
+                    $_additionalLayoutPart = '_new';
+                }
             }
             $this->loadLayout([
                 'default',
@@ -249,10 +254,12 @@ class Mage_Adminhtml_Catalog_ProductController extends Mage_Adminhtml_Controller
         Mage::dispatchEvent('catalog_product_edit_action', ['product' => $product]);
 
         $_additionalLayoutPart = '';
-        if ($product->getTypeId() == Mage_Catalog_Model_Product_Type::TYPE_CONFIGURABLE
-            && !($product->getTypeInstance()->getUsedProductAttributeIds()))
-        {
-            $_additionalLayoutPart = '_new';
+        if ($product->getTypeId() === Mage_Catalog_Model_Product_Type::TYPE_CONFIGURABLE) {
+            /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+            $productType = $product->getTypeInstance();
+            if (!$productType->getUsedProductAttributeIds()) {
+                $_additionalLayoutPart = '_new';
+            }
         }
 
         $this->loadLayout([
@@ -1052,7 +1059,9 @@ class Mage_Adminhtml_Catalog_ProductController extends Mage_Adminhtml_Controller
         $autogenerateOptions = [];
         $result['attributes'] = [];
 
-        foreach ($configurableProduct->getTypeInstance()->getConfigurableAttributes() as $attribute) {
+        /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+        $productType = $product->getTypeInstance();
+        foreach ($productType->getConfigurableAttributes() as $attribute) {
             $value = $product->getAttributeText($attribute->getProductAttribute()->getAttributeCode());
             $autogenerateOptions[] = $value;
             $result['attributes'][] = [

app/code/core/Mage/Adminhtml/Block/Catalog/Product/Composite/Fieldset/Grouped.php
@@ -72,7 +72,9 @@ class Mage_Adminhtml_Block_Catalog_Product_Composite_Fieldset_Grouped extends Ma
     public function getAssociatedProducts()
     {
         $product = $this->getProduct();
-        $result = $product->getTypeInstance(true)->getAssociatedProducts($product);
+        /** @var Mage_Catalog_Model_Product_Type_Grouped $productType */
+        $productType = $product->getTypeInstance(true);
+        $result = $productType->getAssociatedProducts($product);
 
         $storeId = $product->getStoreId();
         foreach ($result as $item) {

app/code/core/Mage/Adminhtml/Block/Catalog/Product/Created.php
@@ -93,7 +93,9 @@ class Mage_Adminhtml_Block_Catalog_Product_Created extends Mage_Adminhtml_Block_
     public function getAttributes()
     {
         if ($this->getConfigurableProduct()->getId()) {
-            return $this->getConfigurableProduct()->getTypeInstance(true)->getUsedProductAttributes($this->getConfigurableProduct());
+            /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+            $productType = $this->getConfigurableProduct()->getTypeInstance(true);
+            return $productType->getUsedProductAttributes($this->getConfigurableProduct());
         }
 
         $attributes = [];

app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit.php
@@ -234,15 +234,18 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit extends Mage_Adminhtml_Block_Wid
 
     public function getIsConfigured()
     {
-        if ($this->getProduct()->isConfigurable()
-            && !($superAttributes = $this->getProduct()
-                ->getTypeInstance(true)
-                ->getUsedProductAttributeIds($this->getProduct()))
-        ) {
-            $superAttributes = false;
+        $superAttributes = true;
+        $product = $this->getProduct();
+        if ($product->isConfigurable()) {
+            /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+            $productType = $product->getTypeInstance(true);
+            $superAttributes = $productType->getUsedProductAttributeIds($product);
+            if (!$superAttributes) {
+                $superAttributes = false;
+            }
         }
 
-        return !$this->getProduct()->isConfigurable() || $superAttributes !== false;
+        return !$product->isConfigurable() || $superAttributes !== false;
     }
 
     public function getSelectedTabId()

app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Super/Config.php
@@ -140,8 +140,9 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config extends Mage_Ad
      */
     public function getAttributesJson()
     {
-        $attributes = $this->_getProduct()->getTypeInstance(true)
-            ->getConfigurableAttributesAsArray($this->_getProduct());
+        /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+        $productType = $this->_getProduct()->getTypeInstance(true);
+        $attributes = $productType->getConfigurableAttributesAsArray($this->_getProduct());
         if(!$attributes) {
             return '[]';
         } else {
@@ -172,9 +173,10 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config extends Mage_Ad
      */
     public function getLinksJson()
     {
-        $products = $this->_getProduct()->getTypeInstance(true)
-            ->getUsedProducts(null, $this->_getProduct());
-        if(!$products) {
+        /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+        $productType = $this->_getProduct()->getTypeInstance(true);
+        $products = $productType->getUsedProducts(null, $this->_getProduct());
+        if (!$products) {
             return '{}';
         }
         $data = [];
@@ -192,8 +194,9 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config extends Mage_Ad
      */
     public function getConfigurableSettings($product) {
         $data = [];
-        $attributes = $this->_getProduct()->getTypeInstance(true)
-            ->getUsedProductAttributes($this->_getProduct());
+        /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+        $productType = $this->_getProduct()->getTypeInstance(true);
+        $attributes = $productType->getUsedProductAttributes($this->_getProduct());
         foreach ($attributes as $attribute) {
             $data[] = [
                 'attribute_id' => $attribute->getId(),
@@ -285,8 +288,9 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config extends Mage_Ad
     protected function _getRequiredAttributesIds()
     {
         $attributesIds = [];
-        $configurableAttributes = $this->_getProduct()
-            ->getTypeInstance(true)->getConfigurableAttributes($this->_getProduct());
+        /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+        $productType = $this->_getProduct()->getTypeInstance(true);
+        $configurableAttributes = $productType->getConfigurableAttributes($this->_getProduct());
         foreach ($configurableAttributes as $attribute) {
             $attributesIds[] = $attribute->getProductAttribute()->getId();
         }

app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Super/Config/Grid.php
@@ -138,8 +138,10 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config_Grid extends Ma
         if (Mage::helper('catalog')->isModuleEnabled('Mage_CatalogInventory')) {
             Mage::getModel('cataloginventory/stock_item')->addCatalogInventoryToProductCollection($collection);
         }
+        /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+        $productType = $product->getTypeInstance(true);
 
-        foreach ($product->getTypeInstance(true)->getUsedProductAttributes($product) as $attribute) {
+        foreach ($productType->getUsedProductAttributes($product) as $attribute) {
             $collection->addAttributeToSelect($attribute->getAttributeCode());
             $collection->addAttributeToFilter($attribute->getAttributeCode(), ['notnull'=>1]);
         }
@@ -158,7 +160,9 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config_Grid extends Ma
     {
         $products = $this->getRequest()->getPost('products', null);
         if (!is_array($products)) {
-            $products = $this->_getProduct()->getTypeInstance(true)->getUsedProductIds($this->_getProduct());
+            /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+            $productType = $this->_getProduct()->getTypeInstance(true);
+            $products = $productType->getUsedProductIds($this->_getProduct());
         }
         return $products;
     }
@@ -183,7 +187,9 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config_Grid extends Ma
     protected function _prepareColumns()
     {
         $product = $this->_getProduct();
-        $attributes = $product->getTypeInstance(true)->getConfigurableAttributes($product);
+        /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+        $productType = $product->getTypeInstance(true);
+        $attributes = $productType->getConfigurableAttributes($product);
 
         if (!$this->isReadonly()) {
             $this->addColumn('in_products', [
@@ -297,11 +303,9 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config_Grid extends Ma
     protected function _getRequiredAttributesIds()
     {
         $attributesIds = [];
-        foreach (
-            $this->_getProduct()
-                ->getTypeInstance(true)
-                ->getConfigurableAttributes($this->_getProduct()) as $attribute
-        ) {
+        /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+        $productType = $this->_getProduct()->getTypeInstance(true);
+        foreach ($productType->getConfigurableAttributes($this->_getProduct()) as $attribute) {
             $attributesIds[] = $attribute->getProductAttribute()->getId();
         }
 
@@ -333,7 +337,9 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config_Grid extends Ma
     {
         if (is_null($this->_configAttributeCodes)) {
             $product = $this->_getProduct();
-            $attributes = $product->getTypeInstance(true)->getConfigurableAttributes($product);
+            /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+            $productType = $product->getTypeInstance(true);
+            $attributes = $productType->getConfigurableAttributes($product);
             $attributeCodes = [];
             foreach ($attributes as $attribute) {
                 $productAttribute = $attribute->getProductAttribute();

app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Super/Config/Simple.php
@@ -58,45 +58,47 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config_Simple
             ->setAttributeSetId($this->_getProduct()->getAttributeSetId())
             ->getAttributes();
 
-        /* Standart attributes */
+        /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
+        $productType = $this->_getProduct()->getTypeInstance(true);
+        $usedAttributes = $productType->getUsedProductAttributes($this->_getProduct());
+
+        /* Standard attributes */
         foreach ($attributes as $attribute) {
+            $attributeCode = $attribute->getAttributeCode();
             if (($attribute->getIsRequired()
-                && $attribute->getApplyTo()
-                // If not applied to configurable
-                && !in_array(Mage_Catalog_Model_Product_Type::TYPE_CONFIGURABLE, $attribute->getApplyTo())
-                // If not used in configurable
-                && !in_array($attribute->getId(),
-                    $this->_getProduct()->getTypeInstance(true)->getUsedProductAttributeIds($this->_getProduct()))
-                )
+                    && $attribute->getApplyTo()
+                    // If not applied to configurable
+                    && !in_array(Mage_Catalog_Model_Product_Type::TYPE_CONFIGURABLE, $attribute->getApplyTo())
+                    // If not used in configurable
+                    && !in_array($attribute->getId(), $usedAttributes))
                 // Or in additional
-                || in_array($attribute->getAttributeCode(), $attributesConfig['additional'])
+                || in_array($attributeCode, $attributesConfig['additional'])
             ) {
                 $inputType = $attribute->getFrontend()->getInputType();
                 if (!in_array($inputType, $availableTypes)) {
                     continue;
                 }
-                $attributeCode = $attribute->getAttributeCode();
                 $attribute->setAttributeCode('simple_product_' . $attributeCode);
                 $element = $fieldset->addField(
                     'simple_product_' . $attributeCode,
-                     $inputType,
-                     [
+                    $inputType,
+                    [
                         'label'    => $attribute->getFrontend()->getLabel(),
                         'name'     => $attributeCode,
                         'required' => $attribute->getIsRequired(),
-                     ]
+                    ]
                 )->setEntityAttribute($attribute);
 
                 if (in_array($attributeCode, $attributesConfig['autogenerate'])) {
                     $element->setDisabled('true');
                     $element->setValue($this->_getProduct()->getData($attributeCode));
                     $element->setAfterElementHtml(
-                         '<input type="checkbox" id="simple_product_' . $attributeCode . '_autogenerate" '
-                         . 'name="simple_product[' . $attributeCode . '_autogenerate]" value="1" '
-                         . 'onclick="toggleValueElements(this, this.parentNode)" checked="checked" /> '
-                         . '<label for="simple_product_' . $attributeCode . '_autogenerate" >'
-                         . Mage::helper('catalog')->__('Autogenerate')
-                         . '</label>'
+                        '<input type="checkbox" id="simple_product_' . $attributeCode . '_autogenerate" '
+                        . 'name="simple_product[' . $attributeCode . '_autogenerate]" value="1" '
+                        . 'onclick="toggleValueElements(this, this.parentNode)" checked="checked" /> '
+                        . '<label for="simple_product_' . $attributeCode . '_autogenerate" >'
+                        . Mage::helper('catalog')->__('Autogenerate')
+                        . '</label>'
                     );
                 }
 
@@ -108,7 +110,6 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config_Simple
         }
 
         /* Configurable attributes */
-        $usedAttributes = $this->_getProduct()->getTypeInstance(true)->getUsedProductAttributes($this->_getProduct());
         foreach ($usedAttributes as $attribute) {
             $attributeCode =  $attribute->getAttributeCode();
             $fieldset->addField( 'simple_product_' . $attributeCode, 'select',  [

commit 975379adf0d795eb9291f591fd4134108c972c43

Fixed bug caused by #2605 (#2625)

app/code/core/Mage/Adminhtml/controllers/Catalog/ProductController.php
@@ -1060,7 +1060,7 @@ class Mage_Adminhtml_Catalog_ProductController extends Mage_Adminhtml_Controller
         $result['attributes'] = [];
 
         /** @var Mage_Catalog_Model_Product_Type_Configurable $productType */
-        $productType = $product->getTypeInstance();
+        $productType = $configurableProduct->getTypeInstance();
         foreach ($productType->getConfigurableAttributes() as $attribute) {
             $value = $product->getAttributeText($attribute->getProductAttribute()->getAttributeCode());
             $autogenerateOptions[] = $value;